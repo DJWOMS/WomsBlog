f30749afe7521cb99a64c93ab4b4a644585472fc
Omelchenko Misha <l2maximum@mail.ru>
2018-03-20 07:10:05 +0300

    List ticket and update ticket

--- ticket/urls.py
+++ ticket/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     # path("", views.MyView.as_view()),
-    path("add-ticket/", views.AddTicket.as_view())
+    path("add-ticket/", views.AddTicket.as_view()),
+    path("list-ticket/", views.ListTicket.as_view(), name="list-ticket"),
+    path("update-ticket/<int:pk>", views.UpdateTicket.as_view(), name="update-ticket"),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import View
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+
+from django.urls import reverse
 
 from .models import Ticket
 from .forms import AddTicketForm
@@ -15,12 +18,35 @@ class AddTicket(CreateView):
     form_class = AddTicketForm
     template_name = "ticket/add-ticket.html"
     
-    
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.save()
         return redirect("/ticket/add-ticket/")
-    
-        
+     
     def success_url(self):
         return redirect("/add-ticket/")
+
+
+class ListTicket(ListView):
+    """Список тикетов пользователя
+    """
+    model = Ticket
+    # queryset = Ticket.objects.all()
+    context_object_name = "tickets"
+    template_name = "ticket/list-ticket.html"
+    
+    def get_queryset(self):
+        return Ticket.objects.filter(user=self.request.user)
+
+ 
+class UpdateTicket(UpdateView):
+    """Редактипрование тикета
+    """
+    model = Ticket
+    form_class = AddTicketForm
+    template_name = "ticket/update-ticket.html"
+
+    def get_success_url(self):
+        return reverse('list-ticket')
+        
+    
